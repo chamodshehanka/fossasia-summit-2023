d2e3ab4edf9388a9a5f8af9aaf52ebbf744c4325
Shehanka <hcsperera@outlook.com>
2023-04-15 07:06:51 +0800

    Application updated

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, FOSSASIA 2023!")
+		fmt.Fprintf(w, "Hello, FOSSASIA 2023! \n")
 	})
 
+	fmt.Println("Starting FOSSASIA 2023 API Server...")
 	fmt.Println("Listening on :8080")
 	http.ListenAndServe(":8080", nil)
 }
